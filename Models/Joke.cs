7e3dfbbc6d36d188412340e99a0bddaea1f1a795
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-03-31 11:22:25 +0100

    init

---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeSetup { get; set; }
+        public string JokePunchline { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
---
+++ dbo.Joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Joke] (
+    [Id]            INT IDENTITY (1, 1) NOT NULL,
+    [JokeSetup]     NVARCHAR(MAX) NOT NULL,
+    [JokePunchline] NVARCHAR(MAX) NOT NULL,
+    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
